c75dc86bfb08e700b80533c6a2164894b44896ca
Codex.bat <serbiaiseverything@gmail.com>
2026-03-14 11:04:27 +0100

    Initial commit

---
+++ src/main/java/net/codex/GroundSlammerClient.java
@@ -0,0 +1,10 @@
+package net.codex;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GroundSlammerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
